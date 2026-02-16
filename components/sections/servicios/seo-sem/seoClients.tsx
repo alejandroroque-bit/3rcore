81fc597cb01617cd6016af39b087c4b5064b0d5e
AymarMolina <aymarmolina9@gmail.com>
2026-02-16 15:18:04 -0500

    O

--- components/sections/servicios/seo-sem/seoClients.tsx
+++ components/sections/servicios/seo-sem/seoClients.tsx
@@ -8,7 +8,7 @@ export function SeoClients() {
   return (
         <section className="relative md:h-[70vh] w-full overflow-hidden">
           <Image
-            src="/images/seosem/image17.png"
+            src="/images/seosem/image17.webp"
             alt="Sobre nosotros"
             fill
             priority
